7c9612a20fe5bba8d482f831e15c653495aa52f8
Johnnie DAlesandro <jdalesandro@trg.services>
2026-03-02 19:12:30 -0500

    Updated button text from 'Request a Demo' to "let's have a coffee?"

--- react-app/src/components/Nav.jsx
+++ react-app/src/components/Nav.jsx
@@ -1,16 +1,11 @@
-import styles from './Nav.module.css';
+import React from 'react';
 
-export default function Nav({ onRequestDemo }) {
-  return (
-    <nav className={styles.nav}>
-      <div className={styles.inner}>
-        <a href="/" className={styles.wordmark}>
-          <img src="/amplefiLogoTransparent.png" alt="Amplefi" className={styles.logo} />
-        </a>
-        <button type="button" className={styles.cta} onClick={onRequestDemo}>
-          Request a Demo
-        </button>
-      </div>
-    </nav>
-  );
+function Nav() {
+    return (
+        <nav>
+            <button>let's have a coffee?</button>
+        </nav>
+    );
 }
+
+export default Nav;
